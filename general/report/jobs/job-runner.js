6ec0e90172f92fc031fa31c947ebb2765ef6f68e
wikimodels <young.wiki.models@gmail.com>
2025-05-23 15:20:20 +0300

    minor fix

--- general/report/jobs/job-runner.js
+++ general/report/jobs/job-runner.js
@@ -29,7 +29,7 @@ function scheduleSelfPing() {
   // Cron expression: second minute hour day month day-of-week
   // '30 0/14 * * * *' → at second 30, every 14 minutes
   const job = new CronJob(
-    "30 */4 * * * *", // note: '*/14' syntax for every 14 minutes
+    "30 */14 * * * *", // note: '*/14' syntax for every 14 minutes
     fetchSelfPongData, // onTick
     null, // onComplete
     true, // start right away
